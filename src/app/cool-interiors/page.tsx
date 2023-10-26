87fc36607fb2bf201f8e6b5ace3fd2aaa794b8ea
Charlie Zhao <charles.zhao5461@gmail.com>
2023-10-27 00:20:51 +0700

    Remove redundant route

--- src/app/cool-interiors/page.tsx
+++ src/app/cool-interiors/page.tsx
@@ -1,13 +1,18 @@
 import Image from "next/image";
-import { ArenaBlock } from "@/app/api/arena/route";
 
 const CoolInteriorsPage = async () => {
-  const domain =
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:3000"
-      : "https://www.charliezhao.com";
-  const res = await fetch(`${domain}/api/arena`);
-  const arenaBlocks: ArenaBlock[] = await res.json();
+  const res = await fetch(
+    `https://api.are.na/v2/search/channels/anything-that-looks-cool?subject=blocks`
+  );
+
+  const data: ArenaChannel = await res.json();
+
+  const arenaBlocks: ArenaBlock[] = data.blocks.map((block) => ({
+    description: block.description,
+    createdAt: block.created_at,
+    title: block.title,
+    srcLarge: block.image.large.url,
+  }));
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-3">
@@ -17,7 +22,7 @@ const CoolInteriorsPage = async () => {
             priority={i < 6}
             className="w-full h-full object-contain"
             src={block.srcLarge}
-            alt=""
+            alt="Photo of a cool interior"
             fill
             sizes="(max-width: 768px) 80vw, 28vw"
           />
@@ -27,3 +32,55 @@ const CoolInteriorsPage = async () => {
   );
 };
 export default CoolInteriorsPage;
+
+interface ArenaBlock {
+  description: string;
+  createdAt: Date;
+  title: string;
+  srcLarge: string;
+}
+
+interface ArenaChannel {
+  blocks: Block[];
+}
+
+interface Block {
+  id: number;
+  title: string;
+  updated_at: Date;
+  created_at: Date;
+  description: string;
+  source: Source | null;
+  image: ArenaImage;
+}
+
+interface ArenaImage {
+  filename: string;
+  updated_at: Date;
+  thumb: Display;
+  square: Display;
+  display: Display;
+  large: Display;
+  original: Original;
+}
+
+interface Display {
+  url: string;
+}
+
+interface Original {
+  url: string;
+  file_size: number;
+  file_size_display: string;
+}
+
+interface Source {
+  url: string;
+  title: null | string;
+  provider: Provider;
+}
+
+interface Provider {
+  name: string;
+  url: string;
+}
