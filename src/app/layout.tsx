76c0f79eb3b420fa325ea057b4e2d79d7bdd7798
Charlie Zhao <charles.zhao5461@gmail.com>
2023-10-27 12:35:25 +0700

    Style scrollable header, make header sticky

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,23 +22,32 @@ export default function RootLayout({
     { href: "/writings", label: "Writings" },
     { href: "/cool-interiors", label: "Interiors" },
   ];
+  const gradientStyle = {
+    background: "-webkit-linear-gradient(left,hsla(0,0%,100%,0),#fff)",
+  };
 
   return (
     <html lang="en">
       <body className={`${inter.className} min-h-screen`}>
-        <header className="flex gap-4 font-light text-sm p-4 fixed top-0 left-0 z-20 bg-white w-full items-center overflow-scroll">
-          <Link href="/" className={`font-bold `}>
-            Charlie <span className="hidden xs:inline">Zhao</span>
-          </Link>
-          <nav className="flex gap-4">
-            {links.map(({ href, label }) => (
-              <Link key={href} href={href} className={`${activeClass}`}>
-                {label}
-              </Link>
-            ))}
-          </nav>
+        <header className="font-light text-sm px-4 sticky top-0 left-0 z-20 bg-white w-full">
+          <div className="flex gap-4 py-4 items-center overflow-scroll">
+            <Link href="/" className={`font-bold `}>
+              Charlie <span className="hidden xs:inline">Zhao</span>
+            </Link>
+            <nav className="flex gap-4">
+              {links.map(({ href, label }) => (
+                <Link key={href} href={href} className={`${activeClass}`}>
+                  {label}
+                </Link>
+              ))}
+            </nav>
+            <div
+              className="w-32 h-full absolute top-0 right-0 pointer-events-none"
+              style={gradientStyle}
+            />
+          </div>
         </header>
-        <main className="font-light text-sm p-4 pt-14">{children}</main>
+        <main className="font-light text-sm px-4">{children}</main>
       </body>
     </html>
   );
