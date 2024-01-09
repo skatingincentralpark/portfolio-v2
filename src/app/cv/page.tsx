8f67017dc7abffc42e5c28ae24ccc17c33c353a9
Charlie Zhao <charles.zhao5461@gmail.com>
2024-01-09 12:32:07 +1100

    Add CV

---
+++ src/app/cv/page.tsx
@@ -0,0 +1,14 @@
+export default function CVPage() {
+  return (
+    <div>
+      <a
+        href="/Charles-Zhao-CV.pdf"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="underline"
+      >
+        Download my CV 📝
+      </a>
+    </div>
+  );
+}
